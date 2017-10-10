4b7580a057725b54a85149c2403adc44194b2dca
Liz <lkalina@ckmadvisors.com>
2017-10-10 18:03:30 -0400

    Adds observations blurb

--- resources/js/choropleth.js
+++ resources/js/choropleth.js
@@ -174,6 +174,24 @@ renders allowing the user to explore on their own.
       .html("Prosper Loan Density by State from 2006 - 2013")
   }
 
+  function addSummary(){
+
+    d3.select('footer')
+      .append('div')
+      .attr('class','sum_cont')
+
+    d3.select('.sum_cont')
+      .append('h1')
+      .attr('class','sum_cont')
+      .html("Observations")
+
+    d3.select('.sum_cont')
+      .append('p')
+      .attr('class','sum_cont')
+      .html(summary_text)
+  }
+
+
   //adds color legend for the map
   function addLegend(){
 
@@ -251,6 +269,7 @@ renders allowing the user to explore on their own.
       if (years[yearIdx] == '2013') {
         clearInterval(yearInterval)
         addSlider();
+        addSummary();
       }
 
       yearIdx++;
@@ -258,6 +277,9 @@ renders allowing the user to explore on their own.
     }, 1000);
 
   }
+
+  //observations summary
+  var summary_text = "Prosper is a peer-to-peer lender and the dataset used includes 113,937 loans with 81 variables (such as loan amount, borrower rate, borrower state) for each loan. I choose to investigate the progression of loan amount by state to get a picture of where the highest loan density occurs. Once I plotted overall loan density, I included a time component to show the geographic fluctuations in lendees. What I discovered was that the loan density traces the story of Prosper and the US economy as a whole; loan density starts out strongest on the West Coast (where Prosper was founded in 2005), then follows the movement of the US economy (with dips in 2008 - 2009)."
 
   //state abbrevation & population lookup
   var abbr = {
